85a143222c4ad5fb6508dc892857df6c2eb12fa0
jamiewan1989 <xiyanw@uci.edu>
2017-02-14 10:22:58 -0800

    Update guesthouse.sql

--- guesthouse.sql
+++ guesthouse.sql
@@ -40,4 +40,26 @@ GROUP BY guest_id;
 
 /* Medium */
 /* Q6 */
+SELECT SUM(nights*amount)
+FROM booking a
+JOIN guest b
+  ON a.guest_id = b.id
+JOIN rate c
+  ON a.room_type_requested = c.room_type AND occupants = occupancy
+WHERE b.first_name = "Ruth" AND b.last_name = "Cadbury";
+
+/* Q7 --> the answer maybe wrong from sqlzoo --> this is what I think is correct*/
+SELECT nights*rate.amount + ex.amount 
+FROM booking b
+JOIN rate
+ON room_type_requested = room_type AND occupants = occupancy
+JOIN 
+    (SELECT booking_id,SUM(amount) as amount 
+     FROM extra 
+     WHERE booking_id = 5128 
+     GROUP BY booking_id) as ex
+ON ex.booking_id = b.booking_id
+WHERE b.booking_id = 5128; 
+
+/* Q8 */
 
